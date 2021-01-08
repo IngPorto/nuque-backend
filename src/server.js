de66347c5fae469e25aa0caa4229b1b2e383e496
david <ing.portocarrero@gmail.com>
2021-01-07 21:47:45 -0500

    cors config and json format acceptance

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const server = express()
 const { mongoose } = require('./database')
+const cors = require('cors')
 
 server.set('port', process.env.PORT || 3001)
+
+server.use(cors())
+server.use(express.json())
 
 server.use('/v1/service', require('./routes/v1/service.routes'))
 server.use('/v1/user', require('./routes/v1/user.routes'))
